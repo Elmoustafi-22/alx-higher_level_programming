a4c9aeb8844d18fe1d779dd6c520781187bd80ec
mustopha <mustophaqadir@gmail.com>
2022-12-06 12:26:37 -0800

    update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-# 10-divisible_by_2.py
-
-
 def divisible_by_2(my_list=[]):
-    """Find all multiples of 2 in a list."""
-    multiples = []
-    for i in range(len(my_list):
-        if my_list[i] % 2 == 0:
-            multiples.append(True)
+    new = []
+    for i in my_list:
+        if i % 2 is 0:
+            new.append(True)
         else:
-            multiples.append(False)		
-    return (multiples)
+            new.append(False)
+        return new
