3bbd6cf35a0e6cecb4a392ca62629f7d2aa76028
mustopha <mustophaqadir@gmail.com>
2022-12-07 10:29:05 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function"""
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function"""
+    return (a - b)
+
+
+def mul(a, b):
+    return (a * b)
+
+
+def div(a, b):
+    return int(a / b)
